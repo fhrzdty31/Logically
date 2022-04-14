27540ee4d9e075bf67645479682a637ff44ae68b
Fahreza Ditya Alfandry <98719648+fhrzdty31@users.noreply.github.com>
2022-04-14 21:07:32 +0700

    Delete Logically02.py

--- Logically02.py
+++
@@ -1,9 +0,0 @@
-# Ganjil atau genap
-
-print("\n\n== Ganjil Genap ==\n\n")
-a = int(input('Masukkan Nilai : '))
-
-if (a % 2) == 0 :
-    print(f'\n=> {a} bernilai Genap\n')
-else :
-    print(f'\n=> {a} bernilai Ganjil\n')
